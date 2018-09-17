6ab39e1e24c1b87c21c13bdfd0d9b94f27104183
Benjamin Bowen <benjamin.bowen@bjss.com>
2018-09-17 15:32:24 +0100

    T8 complete

--- T8/scripts.js
+++ T8/scripts.js
@@ -1,190 +1,110 @@
-recoverCountdowns();
+$(function() {
 
-var secInMin = 60;
-var secInHour = 3600;
+//    loadCountdowns();
 
-var buttonClick = document.querySelector("#startButton");
-buttonClick.addEventListener("click", startButtonPressed, false);
+    $("#startButton").on('click', startButtonPressed);
+    $("#clearButton").on('click', clearButtonPressed);
 
-var clearClick = document.querySelector("#clearButton");
-clearClick.addEventListener("click", clearButtonPressed, false);
+    function startButtonPressed() {
 
-function startButtonPressed(event) {
-    event.preventDefault();
+        var userInput = parseInt($("input[name=userInput]").val(), 10) || 10;
+        var endTime = Date.now() + userInput * 1000;
 
-    //access our form element
-    var durationInputH = document.forms[0].elements[0];
-    var durationInputM = document.forms[0].elements[1];
-    var durationInputS = document.forms[0].elements[2];
+        countdown(endTime);
 
-    //value is in base 10
-    //if we dont have a var in the form, default it to 10
-    var countdownFrom =   [
-      parseInt(durationInputH.value, 10) || 0,
-      parseInt(durationInputM.value, 10) || 0,
-      parseInt(durationInputS.value, 10) || 10];
+        return false;
+    }
 
-    performCountdown(countdownFrom);
-}
+    function clearButtonPressed() {
 
-function clearButtonPressed(event) {
-    event.preventDefault();
-    localStorage.clear();
-    localStorage.removeItem("countdowns");
-    location.reload();
-}
+        localStorage.clear();
+        localStorage.removeItem("countdowns");
+        location.reload();
 
-function performCountdown(duration) {
+    }
 
-    var countdownFrom = duration;
+    function countdown(endTime) {
 
-    //grab my html template and clone it
-    var template = document.querySelector("#template");
-    var newCountdown = template.cloneNode(true);
-    newCountdown.id = null;
-    //place the clone at the top of our "list"
-    template.parentNode.insertBefore(newCountdown, template);
+        if (currentCountdown(endTime) <= 0) return;
 
-    //get the objects from the HTML
-    var countdownElement = [
-        newCountdown.querySelector(".countH"),
-        newCountdown.querySelector(".countM"),
-        newCountdown.querySelector(".countS")
-    ];
-    //get stop button, add event listener
-    var stopButtonElement = newCountdown.querySelector(".stopButton");
-    stopButtonElement.addEventListener("click", stopCountdown, false);
+        var template = $('#template');
+        var newCountdown = template.clone().removeAttr("id").insertBefore(template);
 
-    countdownLoop();
+        var countdownElement = $(".count", newCountdown);
+        countdownElement.attr("endTime", endTime);
 
-    function countdownLoop() {
-        var countdownStateClass = "running";
+        $(".stopButton", newCountdown).on('click', stopCountdown);
 
-        countdownElement[0].setAttribute('countH', countdownFrom[0]);
-        countdownElement[1].setAttribute('countM', countdownFrom[1]);
-        countdownElement[2].setAttribute('countS', countdownFrom[2]);
+        function continueCountdown() {
 
-        //assert text content and design to the page
-        countdownElement[0].innerText = countdownFrom[0];
-        countdownElement[1].innerText = countdownFrom[1];
-        countdownElement[2].innerText = countdownFrom[2];
+            var countdownStateClass = "running";
+            var countdown = currentCountdown(endTime);
+            countdownElement.text(formatCountdown(countdown));
 
-        //check if countdown has ended
-        if (
-            countdownFrom[0] > 0 ||
-            countdownFrom[1] > 0 ||
-            countdownFrom[2] > 0) {
+            if (countdown > 0) {
 
-            if (countdownFrom[0] === 0 &&
-                countdownFrom[1] === 0 &&
-                countdownFrom[2] < 5) {
-                //less than 5? make it orange text
-                countdownStateClass = "ending";
+                if (countdown < 5) {
+
+                    countdownStateClass = "ending";
+
+                }
+
+                setTimeout(continueCountdown, 500);
 
             } else {
-                //5 or more? green text
-                countdownStateClass = "running";
 
-            }
-
-        } else {
-                //0? red text
                 countdownStateClass = "done";
 
-        }
-
-            countdownElement.className = countdownStateClass;
-
-            decrement();
-
-            cacheCountdowns();
-
-        }
-
-        function decrement(){
-
-          //if we have seconds, decrement seconds
-          if (countdownFrom[2] > 0) {
-              countdownFrom[2] -= 1;
-          }
-          //if we dont have seconds
-          else if (countdownFrom[2] == 0) {
-              //check if we have minutes
-              if (countdownFrom[1] > 0) {
-                  //if we have minutes decrement minute
-                  //and give us 59 more seconds
-                  countdownFrom[1] -= 1;
-                  countdownFrom[2] = 59;
-                  //if we dont have a minutes
-                  //check if we have an hour
-              } else if (countdownFrom[1] == 0) {
-                  if (countdownFrom[0] > 0) {
-                      //if we have an hour, give 59 minutes
-                      //and 59 seconds
-                      countdownFrom[0] -= 1;
-                      countdownFrom[1] = 59;
-                      countdownFrom[2] = 59;
-                  }
-              }
-          }
-
-              setTimeout(countdownLoop, 1000);
-
-
-        }
-
-        function stopCountdown(event) {
-
-            event.preventDefault();
-            countdown = 0;
-            template.parentNode.removeChild(newCountdown);
-
-        }
-}
-
-function cacheCountdowns() {
-
-    var allCounters = [];
-    var allCounterElementsS = document.querySelectorAll(".countH");
-    var allCounterElementsM = document.querySelectorAll(".countM");
-    var allCounterElementsH = document.querySelectorAll(".countS");
-
-    for (var i = 0; i < allCounterElementsS.length; i++) {
-
-        var counterElementH = allCounterElementsH.item(i);
-        var counterElementM = allCounterElementsM.item(i);
-        var counterElementS = allCounterElementsS.item(i);
-
-        if (
-            counterElementS.hasAttribute("countH") ||
-            counterElementM.hasAttribute("countM") ||
-            counterElementH.hasAttribute("countS")) {
-
-            var count = [parseInt(counterElementS.attributes["countH"].value, 0),
-                parseInt(counterElementM.attributes["countM"].value, 0),
-                parseInt(counterElementH.attributes["countS"].value, 0)
-            ];
-
-            if (count) {
-                allCounters.push(count);
-
             }
 
+            countdownElement.removeClass().addClass("count " + countdownStateClass);
         }
 
-    }
+        function stopCountdown() {
 
-    window.localStorage.setItem("countdowns", JSON.stringify(allCounters));
+            newCountdown.remove();
+            saveCountdowns();
+            return false;
 
-}
+        }
 
-function recoverCountdowns() {
-    var allCounters = JSON.parse(window.localStorage.getItem("countdowns"));
-
-    if (allCounters && allCounters.forEach) {
-
-        allCounters.forEach(performCountdown);
+      //  saveCountdowns();
+        continueCountdown();
 
     }
 
-}
+    function formatCountdown(countdown){
+
+        var h = Math.floor(countdown / 3600);
+        var m = Math.floor(countdown / 60) - (h * 60);
+        var s = countdown % 60;
+
+        return (h + "H, "+m + "M, "+s + "S");
+    }
+/*
+    function saveCountdowns() {
+        var allCounters = $("has(.count[endDateTime])").map(function () {
+            return parseInt($(".count", this).attr("endDateTime"), 10);
+        }).get().filter(function (counter) {
+            return counter.endDateTime > Date.now();
+        });
+
+        window.localStorage.setItem("countdowns", JSON.stringify(allCounters));
+    }
+
+    function loadCountdowns() {
+        var allCounters = JSON.parse(window.localStorage.getItem("countdowns"));
+        if (allCounters) {
+            allCounters.forEach(function (counter) {
+                countdown(counter.endDateTime);
+            });
+        }
+    }
+*/
+    function currentCountdown(endDateTime) {
+
+        return parseInt((endDateTime - Date.now()) / 1000, 10);
+
+    }
+
+});
